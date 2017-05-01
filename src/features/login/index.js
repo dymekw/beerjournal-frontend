f873712409066dd58e59a5ab2c90120bc485d74c
beatabubula <beatabubula@interia.pl>
2017-05-01 09:47:14 +0200

    add fb login

--- src/features/login/index.js
+++ src/features/login/index.js
@@ -1,10 +1,24 @@
 import angular from "angular";
 import uirouter from "angular-ui-router";
 import routing from "./login.routes";
+import "ng-facebook";
 
 import LoginController from "./login.controller"
 
-export default angular.module('login', [uirouter])
+export default angular.module('login', [uirouter,'ngFacebook'])
     .controller('LoginController', LoginController)
+    .config( function( $facebookProvider ) {
+        $facebookProvider.setAppId('1618894634805119');
+    })
     .config(routing)
+    .run( function( $rootScope ) {
+        (function(){
+            if (document.getElementById('facebook-jssdk')) {return;}
+            var firstScriptElement = document.getElementsByTagName('script')[0];
+            var facebookJS = document.createElement('script');
+            facebookJS.id = 'facebook-jssdk';
+            facebookJS.src = '//connect.facebook.net/en_US/all.js';
+            firstScriptElement.parentNode.insertBefore(facebookJS, firstScriptElement);
+        }());
+    })
     .name;
--- src/features/login/login.controller.js
+++ src/features/login/login.controller.js
@@ -1,7 +1,7 @@
 /**
  * Created by wojciech_dymek on 21.04.17.
  */
-export default function LoginController($rootScope, $http, $location, $sessionStorage, $base64) {
+export default function LoginController($rootScope, $http, $location, $sessionStorage, $base64,$facebook,$scope) {
     let vm = this;
     vm.login = login;
     vm.logout = logout;
@@ -16,7 +16,7 @@ export default function LoginController($rootScope, $http, $location, $sessionSt
             var authdata = $base64.encode(user.username + ':' + user.password);
             $sessionStorage.putObject('user', {username: user.username, id: user.id, auth: authdata});
             $rootScope.globals.currentUser = user;
-
+            console.log(user);
             $http.defaults.headers.common['Authorization'] = 'Basic ' + authdata;
             $location.path('/collections');
         }, function(res) {
@@ -37,4 +37,28 @@ export default function LoginController($rootScope, $http, $location, $sessionSt
             str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
         return str.join("&");
     }
+
+
+    //////FACEBOOK LOGIN//////////////
+
+    $scope.loginFb = function() {
+        $facebook.init();
+        $facebook.login().then(function() {
+            getFacebookUser();
+        });
+
+    }
+    function getFacebookUser() {
+        $facebook.api("/me").then(
+            function(response) {
+                console.log(response);
+            },
+            function(err) {
+                console.log(err);
+            });
+    }
+
+
+
+
 }
