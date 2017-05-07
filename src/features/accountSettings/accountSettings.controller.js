a695f1431678cfcb86070307836daf4133341233
drewniak <aleksandra.drewniak1a@gmail.com>
2017-05-07 19:49:34 +0200

    add modifying and deleting user

--- src/features/accountSettings/accountSettings.controller.js
+++ src/features/accountSettings/accountSettings.controller.js
@@ -1,13 +1,41 @@
-export default function AccountSettingsController($scope, $rootScope, $stateParams, $http, $location, $uibModal, $base64) {
+export default function AccountSettingsController(authService, $scope, $rootScope, $stateParams, $http, $location) {
+    let accountSettings = this;
     $scope.userData = {};
-    $scope.errorMsg;
+  $scope.userData1 = {};
+    $scope.errorMessage;
 
-    $http.get('/api/users/' +  this.userId).then(function(response) {
+    $http.get('/api/users/' +  $rootScope.globals.currentUser.id).then(function(response) {
         $scope.userData = response.data;
     });
+ $http.get('/api/users/' +  $rootScope.globals.currentUser.id).then(function(response) {
+        $scope.userData1 = response.data;
+    });
 
-    $scope.update = function () {
-      
+    $scope.updateUser = function () {  
+        $scope.userData = { 
+             "email": $scope.userData.email,
+
+            "password": $scope.userData.password,
+            "lastName" : $scope.userData.lastName,
+            "firstName": $scope.userData.firstName
+};
+       
+     $http.put('/api/users/' + $rootScope.globals.currentUser.id, this.userData).then(function(res) {
+            $location.path("/accountSettings")
+        }, function(res) {
+            $scope.errorMsg ="Unable to update user";
+        });
+        
+    
     };
+
+
+    $scope.deleteUser = function() {
+        if (confirm("Are you sure?") == true) {
+         $location.path("/home")
+         $http.delete('/api/users/' + $rootScope.globals.currentUser.id);
+            authService.logout();
+    } 
+}
 
 }
