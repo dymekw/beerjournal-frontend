67779f14e049ab3852cd7571c3578b09b8eb3511
Wojciech Dymek <wojciechdymek94@gmail.com>
2017-05-13 17:17:49 +0200

    Events view improvements

--- src/features/events/events.controller.js
+++ src/features/events/events.controller.js
@@ -1,4 +1,4 @@
-export default function EventsController($rootScope, $scope, $http, $location, $uibModal,moment) {
+export default function EventsController($rootScope, $scope, $http, $location, $uibModal, moment) {
     let user = undefined;
     if (!$scope.selectedUser) {
         $scope.selectedUser = $rootScope.globals.currentUser;
@@ -16,12 +16,60 @@ export default function EventsController($rootScope, $scope, $http, $location, $
                 $scope.events = [];
                 events.forEach(function (event) {
                     event.date = moment(event.date).fromNow();
+                    loadEventImage(event);
                     $scope.events.push(event);
                 })
-                console.log("Hi events", $scope);
             }, function (error) {
                 console.log(error);
             });
+    }
+
+    $scope.userCreatedEvent = function(user) {
+        $rootScope.selectedUser = user;
+        $location.path('/allUsers');
+    }
+
+    $scope.itemEvent = function(item) {
+        $scope.itemId = item.id;
+        var modalInstance = $uibModal.open({
+            templateUrl: 'modals/itemDetails.html',
+            scope: $scope
+        }).result.finally(
+            function() {
+            }
+        ).then(angular.noop, angular.noop);
+    }
+
+    function loadEventImage(event) {
+        if (event.dataType == 'ITEM') {
+            var DEFAULT = 'https://image.flaticon.com/icons/svg/410/410321.svg';
+
+            $http.get('/api/items/' + event.data.id).then(
+                function(res){
+                    var imageIds = res.data.imageIds;
+                    if (imageIds.length > 0) {
+                        event.image = '/api/files/' + imageIds[0];
+                    } else {
+                        event.image = DEFAULT;
+                    }
+                }, function() {
+                    event.image = DEFAULT;
+                });
+        } else if(event.dataType == 'USER') {
+            var DEFAULT = 'https://maxcdn.icons8.com/Color/PNG/48/Users/checked_user_male-48.png';
+
+            $http.get('/api/users/' + event.data.id).then(
+                function(res){
+                    if(res.data.avatarFileId) {
+                        event.image = '/api/users/' + event.id + '/avatar';
+                    } else {
+                        event.image = DEFAULT;
+                    }
+                },
+                function(){
+                    event.image = DEFAULT;
+                })
+        }
     }
 
     events();
